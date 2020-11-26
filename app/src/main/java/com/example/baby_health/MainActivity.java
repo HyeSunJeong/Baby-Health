23332c8ad31db97ea1849779b58e6b2159476841
HyeSuni <zaqxswcde88@naver.com>
2020-11-26 14:14:37 +0900

    Calendar View & botton

--- app/src/main/java/com/example/baby_health/MainActivity.java
+++ app/src/main/java/com/example/baby_health/MainActivity.java
@@ -1,14 +1,61 @@
-package com.example.baby_health;
-
-import androidx.appcompat.app.AppCompatActivity;
-
-import android.os.Bundle;
-
 public class MainActivity extends AppCompatActivity {
+
+    //달력 설정
+    DatePickerDialog.OnDateSetListener d = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth){
+            Log.d("YearMonthPickerTest", "year = " + year + ", month = " + monthOfYear + ", day = " + dayOfMonth);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+    }
+}
+
+public class MainActivity extends AppCompatActivity {
+    ...
+
+    //달력 및 버튼들
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        //버튼 가져오기
+        Button editBtn = (Button) findViewById(R.id.edit_button);
+        Button statisticsBtn = (Button) findViewById(R.id.statistics_button);
+        Button stopwatchBtn = (Button) findViewById(R.id.stopwatch_button);
+
+        //"기준 편집"버튼의 이벤트 처리
+        editBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Edit.class);
+                startActivity(intent);
+            }
+        });
+
+        //"공부한 양"버튼의 이벤트 처리
+        statisticsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), Statistics.class);
+                startActivity(intent);
+            }
+        });
+
+        //"오늘 공부시작!"버튼의 이벤트 처리
+        stopwatchBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), StopWatch.class);
+                startActivity(intent);
+            }
+        });
     }
 }
