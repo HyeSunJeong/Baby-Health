f1441ca9481f7a4bcaffe4173921ed5ed7960bfd
HyeSuni <zaqxswcde88@naver.com>
2020-11-29 20:26:57 +0900

    Plan ImageView abs, back, chest

--- app/src/main/java/com/example/baby_health/MainActivity.java
+++ app/src/main/java/com/example/baby_health/MainActivity.java
@@ -1,3 +1,16 @@
+package com.example.baby_health;
+
+import android.app.DatePickerDialog;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.DatePicker;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.example.baby_health.R;
+
+
+
 public class MainActivity extends AppCompatActivity {
 
     //달력 설정
@@ -13,49 +26,5 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-    }
-}
-
-public class MainActivity extends AppCompatActivity {
-    ...
-
-    //달력 및 버튼들
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-
-        //버튼 가져오기
-        Button editBtn = (Button) findViewById(R.id.edit_button);
-        Button statisticsBtn = (Button) findViewById(R.id.statistics_button);
-        Button stopwatchBtn = (Button) findViewById(R.id.stopwatch_button);
-
-        //"기준 편집"버튼의 이벤트 처리
-        editBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), Edit.class);
-                startActivity(intent);
-            }
-        });
-
-        //"공부한 양"버튼의 이벤트 처리
-        statisticsBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), Statistics.class);
-                startActivity(intent);
-            }
-        });
-
-        //"오늘 공부시작!"버튼의 이벤트 처리
-        stopwatchBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent intent = new Intent(getApplicationContext(), StopWatch.class);
-                startActivity(intent);
-            }
-        });
     }
 }
---
+++ app/src/main/java/com/example/baby_health/PlanActivity.java
@@ -0,0 +1,14 @@
+package com.example.baby_health;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PlanActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_plan);
+    }
+}
